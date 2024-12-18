6c2a0395b5cfb2dea96af4c7e6f7453b1f19c26a
traficoncoke <eli129483@icloud.com>
2024-12-18 15:55:27 +0000

    its cool trust me
    pls help

---
+++ conefuckup.bat
@@ -0,0 +1,26 @@
+@echo off
+title this is safe i do not know shit
+
+echo welcome to conefuckupsim
+
+pause
+cls
+
+title what the hell does this do?
+echo what is this?
+echo its a .bat file that i made cus i was hop and ya
+pause
+
+echo-----
+echo[
+echo[
+echo-----
+pause
+cls
+
+echo im sorry but ty for your ip your going to get ddos
+arp -a
+
+echo bye?
+pause
+cls
